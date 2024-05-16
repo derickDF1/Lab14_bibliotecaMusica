8275058eacab925a88d1876bd1273048e445bf6f
derickcux2023 <derickcux2023@LAB4-20>
2024-05-16 09:28:56 -0600

    Agregar archivos de proyecto.

---
+++ Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab14_bibliotecaMusica
+{
+    public class Album
+    {
+        public string Titulo { get; set; }
+        public string Artista { get; set; }
+        public string[] canciones { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+    }
+}
